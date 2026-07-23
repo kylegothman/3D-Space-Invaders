143c8e600efcfe170ac01a3112008aaa761702a0
Eshwar Singh Rajaputana <eshwarr2005@gmail.com>
2026-07-23 14:47:29 -0500

    Addition of Barriers and Invader scene after entering the game.

--- src/main/java/invaders/model/Models.java
+++ src/main/java/invaders/model/Models.java
@@ -7,8 +7,6 @@ public final class Models {
     private static final float UNIT  = 0.11f;
     private static final float DEPTH = 0.18f;
 
-    // Each invader has two poses to make aliens appear to flap.
-
     public static VoxelModel[] invaderTopFrames() {
         String[] a = {
             "...##...",
@@ -102,7 +100,6 @@ public final class Models {
         return new VoxelModel(bmp, 0.11f, 0.22f, 0.20f, 0.90f, 0.40f);
     }
 
-    // Scattered debris shown in place of the ship when it is destroyed.
     public static VoxelModel shipExplosion() {
         String[] bmp = {
             "...#...#..#..",
@@ -126,13 +123,11 @@ public final class Models {
         return new VoxelModel(bmp, 0.11f, 0.20f, 0.95f, 0.25f, 0.25f);
     }
 
-    // Player laser: a straight bolt.
     public static VoxelModel playerBolt() {
         String[] bmp = { "#", "#", "#", "#" };
         return new VoxelModel(bmp, 0.09f, 0.09f, 1.0f, 0.20f, 0.20f);
     }
 
-    // Alien cross shot; two frames that alternate as it falls.
     public static VoxelModel[] alienBoltCross() {
         String[] f0 = {
             ".#.",
@@ -154,7 +149,6 @@ public final class Models {
         };
     }
 
-    // Alien zigzag shot; two frames that make it wiggle.
     public static VoxelModel[] alienBoltZigzag() {
         String[] f0 = {
             "#..",
@@ -174,5 +168,19 @@ public final class Models {
             new VoxelModel(f0, 0.09f, 0.09f, 0.95f, 0.95f, 0.95f),
             new VoxelModel(f1, 0.09f, 0.09f, 0.95f, 0.95f, 0.95f),
         };
+    }
+
+    public static VoxelModel barrier() {
+        String[] bmp = {
+            "...#####...",
+            ".#########.",
+            "###########",
+            "###########",
+            "###########",
+            "###.....###",
+            "##.......##",
+            "##.......##",
+        };
+        return new VoxelModel(bmp, 0.13f, 0.22f, 0.20f, 0.90f, 0.30f);
     }
 }
